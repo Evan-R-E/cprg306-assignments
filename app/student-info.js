b54e6bbdcb638d7821144f2a71a0881178f3d8ca
Evan E <eefifa15@gmail.com>
2023-09-25 18:11:36 -0600

    Week2
    Added week 2

---
+++ app/student-info.js
@@ -0,0 +1,12 @@
+export default function StudentInfo(){
+    return(
+        <main>
+            <div>
+                <p>Name: Evan</p>
+                <p>Course Section: CPRG 306 E</p>
+                <p>https://github.com/Evan-R-E/cprg306-assignments</p>
+
+            </div>
+        </main>
+    );
+}
---
+++ app/week2/page.js
@@ -0,0 +1,13 @@
+import StudentInfo from "/app/student-info";
+
+export default function Page(){
+    return(
+        <main>
+            <div>
+                <h2>My Shopping List</h2>
+                <StudentInfo />
+
+            </div>
+        </main>
+    );
+}
