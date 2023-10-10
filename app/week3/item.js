df4a2e53e43eec933bcce72062b838cacc0ed258
Evan E <eefifa15@gmail.com>
2023-10-09 22:36:52 -0600

    Week4

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,5 +1,5 @@
 export default function Item({ name, quantity, category}) {
-    return(
+    return (
         <div className="border border-purple-500 bg-purple-800 w-full max-w-xs m-4 p-2 rounded-md">
             <h3 className="text-x1 font-bold">Name: {name}</h3>
             <p>Buy {quantity} in {category}</p>
